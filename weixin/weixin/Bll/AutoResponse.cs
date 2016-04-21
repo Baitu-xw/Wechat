9cad945d8a16a8d98c8f06a344d7ae71de308096
itxiaobo <it.xiaobo@gmail.com>
2016-04-21 16:33:30 +0800

    weixin develop

---
+++ weixin/weixin/Bll/AutoResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using weixin.Models;
+
+namespace weixin.Bll
+{
+    public class AutoResponse
+    {
+        public static string xmlStr;
+        public AutoResponse(string Content, string UserName)
+        {
+            string resxml = "<xml> <ToUserName><![CDATA[" + UserName + "]]></ToUserName><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好！开发人员请联系：15258789532]]></Content> </xml>";
+            xmlStr = resxml;
+            AutoResonseMes();
+        }
+        public string AutoResonseMes()
+        {
+            return (xmlStr);
+        }
+    }
+}
---
+++ weixin/weixin/Controllers/weixinController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using weixin.Bll;
+using weixin.Models;
+
+namespace weixin.Controllers
+{
+    public class weixinController : Controller
+    {
+        //
+        // GET: /weixin/
+
+        public ActionResult Index()
+        {
+            string result = "";
+
+            weixin bll = new weixin();
+            string postStr = "";
+            if (Request.HttpMethod.ToLower() == "post")
+            {
+                Stream s = System.Web.HttpContext.Current.Request.InputStream;
+                byte[] b = new byte[s.Length];
+                s.Read(b, 0, (int)s.Length);
+                postStr = Encoding.UTF8.GetString(b);
+
+                if (!string.IsNullOrEmpty(postStr)) //请求处理  
+                {
+                    //封装请求类  
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(postStr);
+                    XmlElement rootElement = doc.DocumentElement;
+                    //MsgType  
+                    XmlNode MsgType = rootElement.SelectSingleNode("MsgType");
+                    //接收的值--->接收消息类(也称为消息推送)  
+                    RequestXML requestXML = new RequestXML();
+                    requestXML.ToUserName = rootElement.SelectSingleNode("ToUserName").InnerText;
+                    requestXML.FromUserName = rootElement.SelectSingleNode("FromUserName").InnerText;
+                    requestXML.CreateTime = rootElement.SelectSingleNode("CreateTime").InnerText;
+                    requestXML.MsgType = MsgType.InnerText;
+
+                    //根据不同的类型进行不同的处理  
+                    switch (requestXML.MsgType)
+                    {
+                        case "text": //文本消息  
+                            requestXML.Content = rootElement.SelectSingleNode("Content").InnerText;
+                            break;
+                        case "image": //图片  
+                            requestXML.PicUrl = rootElement.SelectSingleNode("PicUrl").InnerText;
+                            break;
+                        case "location": //位置  
+                            requestXML.Location_X = rootElement.SelectSingleNode("Location_X").InnerText;
+                            requestXML.Location_Y = rootElement.SelectSingleNode("Location_Y").InnerText;
+                            requestXML.Scale = rootElement.SelectSingleNode("Scale").InnerText;
+                            requestXML.Label = rootElement.SelectSingleNode("Label").InnerText;
+                            break;
+                        case "link": //链接  
+                            break;
+                        case "event": //事件推送 支持V4.5+  
+                            break;
+                    }
+                    result = bll.Handle(requestXML);
+                }
+            }
+            else
+            {
+                bll.Auth();
+            }
+            return Content(result);
+        }
+    }
+}
